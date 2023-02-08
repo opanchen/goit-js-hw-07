ebed3235204f0e92d0d416e03ac3812b181f6d69
opanchen <panchenkooleh@gmail.com>
2023-02-08 13:38:50 +0200

    first task demo solution

---
+++ js/01-gallery.js
@@ -0,0 +1,59 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const refs = {
+  galleryContainer: document.querySelector('.gallery'),
+};
+
+const markup = galleryItems
+  .map(
+    item => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+
+refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+refs.galleryContainer.addEventListener('click', onImageClick);
+
+function onImageClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') return;
+
+  const selectedImgLink = e.target.dataset.source;
+  //   const selectedImgDescr = e.target.alt;
+  //   console.log(`click on "${selectedImgDescr}"\n link: ${selectedImgLink}`);
+
+  const modal = basicLightbox.create(
+    `
+      <div class="modal">
+      <img src="${selectedImgLink}" width="800" height="600">
+      </div>
+      `
+  );
+
+  showModal();
+
+  function showModal() {
+    modal.show();
+    document.addEventListener('keydown', onEsc);
+  }
+
+  function onEsc(e) {
+    // console.log(`Key ${e.key}`);
+    // console.log(`KeyCode ${e.code}`);
+    if (e.code === 'Escape') {
+      modal.close();
+      document.removeEventListener('keydown', onEsc);
+    }
+  }
+}

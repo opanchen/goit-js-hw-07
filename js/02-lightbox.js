1bbb2c4a0f066e2533c3bbb28591fec1c77ee2c9
opanchen <panchenkooleh@gmail.com>
2023-02-08 16:42:34 +0200

    solved task 2 (demo solution) with console error after 'scroll' on modal-img

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const refs = {
+  galleryContainer: document.querySelector('.gallery'),
+};
+
+const markup = galleryItems
+  .map(
+    item => `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>`
+  )
+  .join('');
+
+refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  //   scrollZum: false,
+});
